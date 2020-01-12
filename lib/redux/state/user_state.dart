112fde66bc05471bf0d3bb71242544b750d8753a
Jesse Belcourt <jessebelcourt@gmail.com>
2020-01-11 19:24:46 -0700

    Updated user state to include proper UserModel

--- lib/redux/state/user_state.dart
+++ lib/redux/state/user_state.dart
@@ -1,2 +1,26 @@
-import 'package:meta/meta.dart';
 
+import 'package:meta/meta.dart';
+import 'package:travel_gear_mobile/models/data_models/user_model.dart';
+
+@immutable
+class UserState {
+  final UserModel user;
+
+  UserState({
+    @required this.user,
+  });
+
+  factory UserState.initial() {
+    return new UserState(
+      user: null,
+    );
+  }
+
+  UserState copyWith({
+    UserModel user,
+  }) {
+    return new UserState(
+      user: user ?? this.user,
+    );
+  }
+}
