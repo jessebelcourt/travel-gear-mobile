62215822c4c3b18b65503aad317b49f3e66a7927
Jesse Belcourt <jessebelcourt@gmail.com>
2020-01-11 19:03:14 -0700

    Added account section.

--- lib/ui/views/gear_view.dart
+++ lib/ui/views/gear_view.dart
@@ -13,6 +13,14 @@ class _GearViewState extends State<GearView> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.greenAccent,
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          onPressed: this._addGear,
+        ),
         bottomNavigationBar: Container(
           color: Colors.black,
           padding: EdgeInsets.symmetric(
@@ -20,20 +28,92 @@ class _GearViewState extends State<GearView> {
           ),
           child: _buildBottomTabBar(),
         ),
-        appBar: AppBar(
-          title: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Gear'),
-              ],
-            ),
+        appBar: _buildAppBar(),
+        body: Container(
+          child: TabBarView(
+            children: <Widget>[
+              _buildGearListView(),
+              _buildAccountView(),
+            ],
           ),
         ),
-        body: TabBarView(
+      ),
+    );
+  }
+
+  Container _buildAccountView() {
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          Container(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  30,
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30),
+                            ),
+                          ),
+                          height: 150,
+                          child: Icon(
+                            Icons.account_circle,
+                            size: 68,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    child: Text('profile'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildGearListView() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 15,
+      ),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Icon(Icons.list),
-            Icon(Icons.list),
+            Text('Gear'),
           ],
         ),
       ),
@@ -51,11 +131,15 @@ class _GearViewState extends State<GearView> {
         ),
         Tab(
           icon: Icon(
-            Icons.list,
+            Icons.account_circle,
             color: Colors.white,
           ),
         ),
       ],
     );
+  }
+
+  void _addGear() {
+    print('add gear.....');
   }
 }
